b6d51a240a83e8f726f7e64c05dc83e212773d5b
Panndora1 <90084527+Panndora1@users.noreply.github.com>
2022-02-24 12:24:57 +0400

    answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+  let str = '' + n;
+
+    if (str[0] == '-') {
+        str = str.slice(1, str.length);
+    }
   
+    str = str.split('').reverse().join('');
+    return Number(str)
 }
